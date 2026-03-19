c7323882be4d71fb427022785f34fa2e7223ca43
Eder Fernando Torres <86902176+eftorres-embedded@users.noreply.github.com>
2026-03-18 21:49:47 -0500

    Update axi_lite_pwm.sv
    adjusted to design the micro architecture with a moore fsm.

--- rtl/bus/axi_lite_pwm.sv
+++ rtl/bus/axi_lite_pwm.sv
@@ -83,6 +83,11 @@ module  axi_lite_pwm    #(
 
     //---------------------------------------------------------
     //AXI response encodings
+    //AXI4-Lite uses a 2-bit response field on both the write-response
+    //(BRESP) and the read-data channel (RRESP). 
+    //We are only using :
+    //OKAY = normal successful transaction
+    //SLVERR = peripheral-reported error
     //---------------------------------------------------------
     localparam  logic   [1:0]   AXI_RESP_OKAY       =   2'b00;
     localparam  logic   [1:0]   AXI_RESP_SLVERR     =   2'b10;
@@ -103,26 +108,84 @@ module  axi_lite_pwm    #(
     logic                           rsp_err;   
 
     //----------------------------------------------------------
-    //AXI channel holding registers
-    //----------------------------------------------------------
-    logic                           aw_hold_valid;
-    logic   [AXI_ADDR_W-1:0]        aw_hold_addr;
+    //State encoding
+    //This Wrapper is intentionally implemented as a single Moore FSM:
+    //
+    // - outputs are driven from the current state
+    // - payloads are held in registers
+    // - reads and writes are serialized
+    //
+    // States:
+    // 
+    //IDLE: No transaction in progress.
+    //
+    //WR_WAIT_DATA: Write data has been accepted, but write address
+    //has not yet been accepted.
+    //
+    //WR_ISSUE: Drive a write request into pwm_subsystem
+    //
+    //WR_WAIT_RSP: Wait for pwm_subsystem to produce a wirte response
+    //
+    //WR_SEND_B: Present AXI write response to the master
+    //
+    //RD_ISSUE: Drive a read request into pwm_subsystem
+    //
+    //RD_WAIT_RSP: Wait for pwm_subsystem to produce read data/response
+    //
+    //RD_SEND_R: Present AXI read data/response to the master.
+    //------------------------------------------------------------
+    typedef enum logic  [3:0]
+    {
+        IDLE,
+        WR_WAIT_DATA,
+        WR_WAIT_ADDR,
+        WR_ISSUE,
+        WR_WAIT_RSP,
+        WR_SEND_B,
+        RD_ISSUE,
+        RD_WAIT_RSP,
+        RD_SEND_R
+    }   state_t;
 
-    logic                           w_hold_valid;
-    logic   [AXI_DATA_W-1:0]        w_hold_data;
-    logic   [AXI_DATA_W/8)-1:0]     w_hold_strb;
+    state_t current_state, next_state;
+
+    //----------------------------------------------------------
+    //AXI payload holding register
+    //Because AXI channels are decoupled, address and data may arrive on different cycles.
+    //
+    //So they are held until the internal MMIO request can be issued.
+    //----------------------------------------------------------
+    logic   [AXI_ADDR_W-1:0]        awaddr_reg;
+    logic   [AXI_DATA_W-1:0]        wdata_reg;
+    logic   [(AXI_DATA_W/8)-1:0]    wstrb_reg;
+
+    logic   [AXI_ADDR_W-1:0]        araddr_reg;
+
+    //----------------------------------------------------------
+    //AXI response holding registers
+    //
+    //These registers store the response returned by pwm_subsystem so it can
+    //be presented cleanly on AXI side during the response states.
+    //-----------------------------------------------------------
+    logic   [1:0]                   bresp_reg;
+    logic   [AXI_DATA_W-1:0]        rdata_reg;
+    logic   [1:0]                   rresp_reg;
+
+    //-----------------------------------------------------------
+    //Local handshake helpers
+    //Signals to indicate if a handsahke happened on this cycle.
+    //Handshake occurs only when VALID and READY are both high in 
+    //the same cycle
+    //-----------------------------------------------------------
+    logic aw_fire, w_fire, ar_fire, b_fire, r_fire;
+
+    assign  aw_fire =   s_axil_awvalid  &&  s_axil_awready;
+    assign  w_fire  =   s_axil_wvalid   &&  s_axil_wready;
+    assign  ar_fire =   s_axil_arvalid  &&  s_axil_arready;
+    assign  b_fire  =   s_axil_bvalid   &&  s_axil_bready;
+    assign  r_fire  =   s_axili_rvalid  &&  s_axil_rreday;
+
     
-    logic                           ar_hold_valid;
-    logic   [AXI_ADDR_W-1:0]        ar_hold_addr;
-
-    //----------------------------------------------------------
-    //Internal transaction tracking
-    //----------------------------------------------------------
-    logic                           wr_req_inflight;
-    logic                           rd_req_inflight;
-
-    logic                           issue_write_req;
-    logic                           issue_read_req;
 
     //----------------------------------------------------------
     //Instatiate PWM subsystem
@@ -153,11 +216,7 @@ module  axi_lite_pwm    #(
         .period_end(period_end),
         .pwm_raw(pwm_raw));  
 
-        //-----------------------------------------------------------
-        //AXI ready generation
-        //
-        //Only accept:
-        // - AW when no stored AW and no write request/response in progress 
+        
 
 
 
